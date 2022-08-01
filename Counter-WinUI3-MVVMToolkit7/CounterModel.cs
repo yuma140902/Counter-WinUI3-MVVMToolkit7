f1cd652707bab163658bce6e03fc93280327c1ce
yuma140902 <yuma140902@gmail.com>
2022-08-01 12:32:13 +0900

    init

---
+++ Counter-WinUI3-MVVMToolkit7/CounterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter_WinUI3_MVVMToolkit7
+{
+    internal class CounterModel
+    {
+        public int Counter = 0;
+    }
+}
---
+++ Counter-WinUI3-MVVMToolkit7/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Counter_WinUI3_MVVMToolkit7
+{
+    internal class ViewModel : ObservableObject
+    {
+        public ViewModel()
+        {
+            CountUpCommand = new RelayCommand(() => ++Count);
+            ResetCommand = new RelayCommand(() => Count = 0);
+        }
+
+        private CounterModel counter = new CounterModel();
+        public int Count
+        {
+            get => counter.Counter;
+            set => SetProperty(counter.Counter, value, counter, (c, v) => c.Counter = v);
+        }
+
+        public ICommand CountUpCommand { get; }
+        public ICommand ResetCommand { get; }
+    }
+}
